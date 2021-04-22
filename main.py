5d6f39b156d31ab25adaa79330fcffa1d955bdf0
mnedo <basecam@yandex.ru>
2021-04-22 05:59:44 +0300

    vishenka na torte prepare to heroku

--- main.py
+++ main.py
@@ -397,6 +397,6 @@ def news():
 
 
 if __name__ == '__main__':
-    # app.run()
-    # port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run() для тестирования
